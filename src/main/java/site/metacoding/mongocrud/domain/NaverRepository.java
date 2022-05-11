c7c24b236f8f1ea7ed218e29044d3b24d35e4f68
a01048197443 <a01048197443@gmail.com>
2022-05-11 12:41:24 +0900

    몽고DB + 스프링부트 + 통합테스트 + 네이티브쿼리

---
+++ src/main/java/site/metacoding/mongocrud/domain/NaverRepository.java
@@ -0,0 +1,11 @@
+package site.metacoding.mongocrud.domain;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+public interface NaverRepository extends MongoRepository<Naver, String> {
+    @Query("{title : ?0, receiver : ?1}")
+    List<Naver> mFindByTitleAndCompany(String title, String company);
+}
